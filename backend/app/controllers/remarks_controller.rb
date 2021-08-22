26dfebadd3f4149c343542177c7b3cf561dc7e6a
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-08-22 14:41:13 -0400

    building out controllers CRUD

--- backend/app/controllers/remarks_controller.rb
+++ backend/app/controllers/remarks_controller.rb
@@ -1,4 +1,17 @@
 class RemarksController < ApplicationController
 
-    
+    def index 
+    end 
+
+    def show
+    end 
+
+    def create
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
 end
